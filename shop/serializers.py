0dded05f8083353fa7bd9acccad3729ad67ee47a
Joan_E <joanevans18@gmail.com>
2020-10-23 08:32:13 +0300

    orders endpoint

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Order, OrderItem
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Order
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
         fields = '__all__'
--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .api import item_list,item_detail
+from .api import item_list,item_detail, OrderCreate, OrderRetrieveUpdateDestory, add_to_cart, remove_from_cart, get_cart_items
 
 urlpatterns = [
     path('api/items-list',item_list),
-    path('api/item-detail/<str:item_id>',item_detail)
+    path('api/item-detail/<str:item_id>',item_detail),
+    path('api/order/create', OrderCreate.as_view()),
+    path('api/order/<int:id>', OrderRetrieveUpdateDestory.as_view()),
+    path('api/item/<str:item_id>/add_to_cart', add_to_cart),
+    path('api/item/<str:item_id>/remove_from_cart', remove_from_cart),
+    path('api/item/all_items', get_cart_items)
 ]
